730b5016aca2a0687bc2fef3d4146125828d95fa
luisro369 <00005513@uca.edu.sv>
2018-03-22 23:36:08 -0600

    probando si funcionan contadores en java

--- app/src/main/java/com/luisro00005513/tarealabo2/MainActivity.java
+++ app/src/main/java/com/luisro00005513/tarealabo2/MainActivity.java
@@ -3,10 +3,11 @@ package com.luisro00005513.tarealabo2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int contadorClickBoton1 = 0,contadorClickBoton2 = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,18 +15,26 @@ public class MainActivity extends AppCompatActivity {
     }// onCreate class
 
     //======================metodos====================
-    public void darClick(View boton){
+    public void darClick(View v){
+        Button buttonIdplayer1, buttonIdplayer2;
         TextView textContadorPlayer1, textContadorPlayer2;
-        int contadorClickBoton1 = 0,contadorClickBoton2 = 0;
         //------------------------
+        buttonIdplayer1 = findViewById(R.id.button_idplayer1);
+        buttonIdplayer2 = findViewById(R.id.button_idplayer2);
+
         textContadorPlayer1 = findViewById(R.id.text_contador_player1);
         textContadorPlayer2 = findViewById(R.id.text_contador_player2);
-        if (boton == textContadorPlayer1){
+        //------------------------
+        if (v == buttonIdplayer1){
             contadorClickBoton1 += 1;
-            textContadorPlayer1.setText((String)contadorClickBoton1);
-            textContadorPlayer1.setText();
+            textContadorPlayer1.setText(String.valueOf(contadorClickBoton1 * 15));
 
         }//boton1
+        if (v == buttonIdplayer2){
+            contadorClickBoton2 += 1;
+            textContadorPlayer2.setText(String.valueOf(contadorClickBoton2 * 15));
+
+        }//boton2
 
     }//darClick
 }//main class
