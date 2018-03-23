c79f4ad646d85fa6e70554eb7a941a2dc358a8ce
luisro369 <00005513@uca.edu.sv>
2018-03-23 14:19:00 -0600

    ya funciona el contador para los puntos, pero falta asignarle la funcion de ganar el juego (6 juegos - 1 set)

--- app/src/main/java/com/luisro00005513/tarealabo2/MainActivity.java
+++ app/src/main/java/com/luisro00005513/tarealabo2/MainActivity.java
@@ -1,16 +1,17 @@
 package com.luisro00005513.tarealabo2;
 
 import android.app.Application;
+import android.support.annotation.ColorRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 //===============contador pensado para un set(6 juegos)===============
 public class MainActivity extends AppCompatActivity {
-    int contadorClickBoton1 = 0,contadorClickBoton2 = 0,
-            contadorJuegoPlayer1 = 0,contadorJuegoPlayer2 = 0 ;
+    int contadorClickBoton1 = 0,contadorClickBoton2 = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +22,19 @@ public class MainActivity extends AppCompatActivity {
     public void darClick(View v) {
         Button buttonIdplayer1, buttonIdplayer2;
         TextView textContadorPlayer1, textContadorPlayer2;
+        ImageView jugador1, jugador2;
         //------------------------
+        jugador1 = findViewById(R.id.image_id1);
+        jugador2 = findViewById(R.id.image_id2);
+
         buttonIdplayer1 = findViewById(R.id.button_idplayer1);
         buttonIdplayer2 = findViewById(R.id.button_idplayer2);
 
         textContadorPlayer1 = findViewById(R.id.text_contador_player1);
         textContadorPlayer2 = findViewById(R.id.text_contador_player2);
+        //-------------------------
+        jugador1.setBackgroundResource(R.color.Juego);
+        jugador2.setBackgroundResource(R.color.Juego);
         //------------------------
         if (v == buttonIdplayer1) {
             contadorClickBoton1 += 1;
@@ -36,11 +44,58 @@ public class MainActivity extends AppCompatActivity {
             contadorClickBoton2 += 1;
             textContadorPlayer2.setText(String.valueOf(contadorClickBoton2 * 15));
         }//boton2
-        if((contadorClickBoton1 > 3) || (contadorClickBoton2 > 3)){
-            contadorClickBoton1 = 0;
-            contadorClickBoton2 = 0;
+        if((contadorClickBoton1 >= 3) || (contadorClickBoton2 >= 3)){
+            if((contadorClickBoton1 & contadorClickBoton2) == 3){
+                textContadorPlayer1.setText("Deuce");
+                textContadorPlayer2.setText("Deuce");
+            }//if de si ambos estan empatados con 45
+            //----------------------------------------
+            if(contadorClickBoton1 == 4 & contadorClickBoton2 == 3){
+                textContadorPlayer1.setText("advantage");
+                textContadorPlayer2.setText(String.valueOf(contadorClickBoton2 * 15));
+
+                if(contadorClickBoton2 == 4){
+                    contadorClickBoton1 = 3;
+                    contadorClickBoton2 = 3;
+                    textContadorPlayer1.setText(String.valueOf(contadorClickBoton1 * 15));
+                    textContadorPlayer2.setText(String.valueOf(contadorClickBoton2 * 15));
+                }//si el jugdor 2 mete punto le quita el advantage al jugador 1
+            }//si anota un punto mas el jugador1
+            if(contadorClickBoton2 == 4 & contadorClickBoton1 == 3){
+                textContadorPlayer2.setText("advantage");
+                textContadorPlayer1.setText(String.valueOf(contadorClickBoton1 * 15));
+
+                if(contadorClickBoton1 == 4){
+                    contadorClickBoton1 = 3;
+                    contadorClickBoton2 = 3;
+                    textContadorPlayer1.setText(String.valueOf(contadorClickBoton1 * 15));
+                    textContadorPlayer2.setText(String.valueOf(contadorClickBoton2 * 15));
+                    //textContadorPlayer1.setText("advantage");
+                }//si el jugdor 1 mete punto le quita el advantage al jugador 2
+            }//si anota un punto mas el jugador1
+            if(contadorClickBoton1 >= 4){
+                //jugador1.setBackgroundColor(R.color.ganadorJuego);
+                jugador1.setBackgroundResource(R.color.ganadorJuego);
+                resetear(textContadorPlayer1,textContadorPlayer2, jugador1, jugador2);
+            }//si gana el juego jugador1
+            if(contadorClickBoton2 >= 4){
+                //jugador1.setBackgroundColor(R.color.ganadorJuego);
+                jugador2.setBackgroundResource(R.color.ganadorJuego);
+                resetear(textContadorPlayer1,textContadorPlayer2,jugador1, jugador2);
+            }//si gana el juego jugador1
+            /*
+            else{
+            */
         }//if contador juego
     }//darClick
+
+    public void resetear(TextView textContadorPlayer1, TextView textContadorPlayer2, ImageView jugador1, ImageView jugador2){
+        contadorClickBoton1 = 0;
+        textContadorPlayer1.setText(String.valueOf(contadorClickBoton1 * 15));
+        contadorClickBoton2 = 0;
+        textContadorPlayer2.setText(String.valueOf(contadorClickBoton2 * 15));
+    }//else de cuando un jugador gana limpia mente el juego
+
 
 }//main class
 
