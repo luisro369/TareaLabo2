7904e02567d6aff916d85c7099a0c31e982c619b
luisro369 <00005513@uca.edu.sv>
2018-03-22 22:38:05 -0600

    probando si funcionan contadores en java

--- app/src/main/java/com/luisro00005513/tarealabo2/MainActivity.java
+++ app/src/main/java/com/luisro00005513/tarealabo2/MainActivity.java
@@ -2,6 +2,8 @@ package com.luisro00005513.tarealabo2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }// onCreate class
 
+    //======================metodos====================
+    public void darClick(View boton){
+        TextView textContadorPlayer1, textContadorPlayer2;
+        int contadorClickBoton1 = 0,contadorClickBoton2 = 0;
+        //------------------------
+        textContadorPlayer1 = findViewById(R.id.text_contador_player1);
+        textContadorPlayer2 = findViewById(R.id.text_contador_player2);
+        if (boton == textContadorPlayer1){
+            contadorClickBoton1 += 1;
+            textContadorPlayer1.setText((String)contadorClickBoton1);
+            textContadorPlayer1.setText();
+
+        }//boton1
+
+    }//darClick
 }//main class
