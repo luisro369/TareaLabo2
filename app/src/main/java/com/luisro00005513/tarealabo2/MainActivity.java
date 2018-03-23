7f0ace7a26158ce8620865320f0ea7b2b3848e4c
luisro369 <00005513@uca.edu.sv>
2018-03-23 15:46:53 -0600

    probando juego

--- app/src/main/java/com/luisro00005513/tarealabo2/MainActivity.java
+++ app/src/main/java/com/luisro00005513/tarealabo2/MainActivity.java
@@ -11,14 +11,14 @@ import android.widget.TextView;
 
 //===============contador pensado para un set(6 juegos)===============
 public class MainActivity extends AppCompatActivity {
-    int contadorClickBoton1 = 0,contadorClickBoton2 = 0;
+    int contadorClickBoton1 = 0,contadorClickBoton2 = 0, contadorJuego1 = 0, contadorJuego2 = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }// onCreate class
 
-    //======================metodos====================
+    //===============================metodo dar click ===============================================
     public void darClick(View v) {
         Button buttonIdplayer1, buttonIdplayer2;
         TextView textContadorPlayer1, textContadorPlayer2;
@@ -76,25 +76,28 @@ public class MainActivity extends AppCompatActivity {
             if(contadorClickBoton1 >= 4){
                 //jugador1.setBackgroundColor(R.color.ganadorJuego);
                 jugador1.setBackgroundResource(R.color.ganadorJuego);
+                contadorJuego1 += 1;
                 resetear(textContadorPlayer1,textContadorPlayer2, jugador1, jugador2);
             }//si gana el juego jugador1
             if(contadorClickBoton2 >= 4){
                 //jugador1.setBackgroundColor(R.color.ganadorJuego);
                 jugador2.setBackgroundResource(R.color.ganadorJuego);
+                contadorJuego2 += 1;
                 resetear(textContadorPlayer1,textContadorPlayer2,jugador1, jugador2);
             }//si gana el juego jugador1
-            /*
-            else{
-            */
         }//if contador juego
     }//darClick
+    //===============================metodo dar click (fin)===============================================
+
+    //===============================metodo resetear variables ===============================================
 
     public void resetear(TextView textContadorPlayer1, TextView textContadorPlayer2, ImageView jugador1, ImageView jugador2){
         contadorClickBoton1 = 0;
         textContadorPlayer1.setText(String.valueOf(contadorClickBoton1 * 15));
         contadorClickBoton2 = 0;
         textContadorPlayer2.setText(String.valueOf(contadorClickBoton2 * 15));
-    }//else de cuando un jugador gana limpia mente el juego
+    }//resetear
+    //===============================metodo resetear variables (fin)===============================================
 
 
 }//main class
