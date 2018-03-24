36aa3756765e0413921d4a6db8ad485e7e7cc9dd
luisro369 <00005513@uca.edu.sv>
2018-03-23 19:24:58 -0600

    ya funciona!!! en busca de bugs usando el simulador de tenis de GTA5

--- app/src/main/java/com/luisro00005513/tarealabo2/MainActivity.java
+++ app/src/main/java/com/luisro00005513/tarealabo2/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 //===============contador pensado para un set(6 juegos)===============
 public class MainActivity extends AppCompatActivity {
-    int contadorClickBoton1 = 0,contadorClickBoton2 = 0, contadorJuego = 1,contadorJuego1 = 1, contadorJuego2 = 1;
+    int contadorClickBoton1 = 0,contadorClickBoton2 = 0, contadorJuego = 0,contadorJuego1 = 0, contadorJuego2 = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +20,12 @@ public class MainActivity extends AppCompatActivity {
 
     //===============================metodo dar click ===============================================
     public void darClick(View v) {
-        Button buttonIdplayer1, buttonIdplayer2;
-        TextView textContadorPlayer1, textContadorPlayer2, juego;
+        Button buttonIdplayer1, buttonIdplayer2, buttonGanador;
+        TextView textContadorPlayer1, textContadorPlayer2, juego, textGanador;
         ImageView jugador1, jugador2;
         //------------------------
+        textGanador = findViewById(R.id.nombrarGanador);
+        buttonGanador = findViewById(R.id.botonGanador);
         juego = findViewById(R.id.juegoId);
         jugador1 = findViewById(R.id.image_id1);
         jugador2 = findViewById(R.id.image_id2);
@@ -36,7 +38,11 @@ public class MainActivity extends AppCompatActivity {
         //-------------------------
         jugador1.setBackgroundResource(R.color.Juego);
         jugador2.setBackgroundResource(R.color.Juego);
-        //------------------------
+        //--------------condiciones iniciales----------
+        textGanador.setText("");
+        buttonGanador.setBackgroundResource(R.color.Juego);
+        buttonGanador.setText("");
+        //-------------condiciones iniciales(fin)------
         if (v == buttonIdplayer1) {
             contadorClickBoton1 += 1;
             if(contadorClickBoton1 < 4) {
@@ -99,9 +105,23 @@ public class MainActivity extends AppCompatActivity {
             }//if contador click
         }//contadorJuego
         else{
-            resetear(textContadorPlayer1,textContadorPlayer2,jugador1, jugador2);
-            contadorJuego = 0;
-        }
+            if((contadorJuego1 - contadorJuego2 == 2) || (contadorJuego2 - contadorJuego1 == 2)){
+                contadorJuego = 0;
+                juego.setText("juego " + String.valueOf(contadorJuego));
+                if((contadorJuego1 >= 4) & (contadorJuego1 > contadorJuego2)){
+                    textGanador.setText("Jugador 1 gana!!!");
+                }//si gana el jugador 1
+                if((contadorJuego2 >= 4) & (contadorJuego2 > contadorJuego1)){
+                    textGanador.setText("Jugador 2 gana!!!");
+                }//si gana el jugador 1
+                buttonGanador.setBackgroundResource(R.color.ganadorJuego);
+                buttonGanador.setText("Volver a jugar?");
+                resetear(textContadorPlayer1,textContadorPlayer2,jugador1, jugador2);
+            }//si la diferencia entre ambos es 2
+            else{
+                contadorJuego = 5;
+            }
+        }// define quien es el ganador, en 1 set cada jugador por lo menos tiene que ganar 4 partidas
     }//darClick
     //===============================metodo dar click (fin)===============================================
 
